f20d199cccd1cb2429325d66bdca9f06aedbe3aa
Renata <renata.nesio2@gmail.com>
2023-03-05 08:43:03 -0300

    Style header

--- alura-books/src/App.js
+++ alura-books/src/App.js
@@ -3,7 +3,7 @@ import Logo from './componentes/Logo'
 function App() {
   return (
     <div className='App'>
-      <header className='App-heade'>
+      <header className='App-header'>
         <Logo></Logo>
       </header>
     </div>
--- alura-books/src/componentes/Logo/index.js
+++ alura-books/src/componentes/Logo/index.js
@@ -3,7 +3,11 @@ import logo from '../../imagens/logo.svg'
 
 function Logo() {
     return (<div className='logo'>
-        <img src={logo} alt='logo '></img>
+        <img
+            src={logo}
+            alt='logo '
+            className='logo-img'
+        ></img>
         <p><strong>Alura</strong>Books</p>
     </div>)
 }
